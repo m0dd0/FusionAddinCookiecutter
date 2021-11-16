622ae6b2fd5dd439cd5bd34e604da9b83a80cfbd
Moritz <mo.hesche@gmail.com>
2021-11-16 23:07:29 +0100

    submodule fix

---
+++ README.md
@@ -0,0 +1,1 @@
+Use 'cookiecutter https://github.com/m0dd0/FusionAddinBase.git --checkout cookiecutter' to ready to use template for Fusion360 addins.
---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,2 @@
+# submodules are not loaded while cloning and therfore need to be reloaded
+git submodule add https://github.com/m0dd0/fusion_addin_framework.git
