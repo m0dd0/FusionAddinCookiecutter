05104eb878c32ede3a5ccf9794717c65378d31be
Moritz <mo.hesche@gmail.com>
2021-11-17 22:01:54 +0100

    checking out develop branch for faf

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,6 @@
 git init
 # submodules are not loaded while cloning and therfore need to be reloaded
-git submodule add https://github.com/m0dd0/fusion_addin_framework.git
+git submodule add -b develop https://github.com/m0dd0/fusion_addin_framework.git
 git add *
 git commit -a -m "Created ready to use template with https://github.com/m0dd0/FusionAddinBase"
 code .
