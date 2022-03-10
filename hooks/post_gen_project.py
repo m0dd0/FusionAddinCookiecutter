73dabd55695a76fe97d8bc8f8b957f672d5be648
Moritz <mo.hesche@gmail.com>
2022-03-10 22:10:55 +0100

    pythonize and extended post gen script

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,28 @@
+# pylint:disable=subprocess-run-check
+
+import subprocess
+
+subprocess.run(["git", "init"])
+subprocess.run(["git", "checkout", "-b", "main"])
+subprocess.run(
+    [
+        "git",
+        "submodule",
+        "add",
+        "-b",
+        "develop",
+        "https://github.com/m0dd0/fusion_addin_framework.git",
+    ],
+    cwd="./{{cookiecutter.addin_name}}",
+)
+# git submodule update --init --recursive
+subprocess.run(["git", "add", "*"])
+subprocess.run(
+    [
+        "git",
+        "commit",
+        "-a",
+        "-m",
+        "Created ready to use template with https://github.com/m0dd0/FusionAddinBase",
+    ]
+)
--- hooks/post_gen_project.sh
+++
@@ -1,6 +0,0 @@
-git init
-# submodules are not loaded while cloning and therfore need to be reloaded
-git submodule add -b develop https://github.com/m0dd0/fusion_addin_framework.git
-git add *
-git commit -a -m "Created ready to use template with https://github.com/m0dd0/FusionAddinBase"
-code .
